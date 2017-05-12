e5b368f9cd4ade86f2c06aa030c8700f34483c97
julanoff <julanoff@users.noreply.github.com>
2017-05-12 10:23:47 -0400

    Add files via upload

---
+++ chfile.pl
@@ -0,0 +1,33 @@
+#!/usr/local/bin/perl
+# /*********************************************************************/
+
+use Switch;
+
+MAIN:
+$fn = "$ARGV[0]";
+$output_dir	= "$ENV{'AREA_ROOT_DIR'}/output";
+$area_name 	= uc("$ENV{'AREA_NAME'}");
+$src="";
+
+$que1 = "$ARGV[0]";
+
+# 
+#open(OUTALL, ">${output_dir}/F50F_report_$cur_date.csv") || die "Can't open ${output_dir}/\n";
+open(INALL, "<$fn") || die "Can't open '$fn' /\n";
+open(OUTALL, ">$fn.out") || die "Could not open file '$fn' $!\n";
+flock(OUTALL, 2) || die;
+	
+while (my $line = <INALL>)  {
+	chomp($line);
+	if ($line eq "-}") {
+	  print OUTALL "$line\n";
+	}
+	else {
+  	print OUTALL "$line\r\n";
+	}
+}
+flock(OUTALL, 8) || die;
+close(OUTALL);
+close(INALL);
+
+exit  (1);
